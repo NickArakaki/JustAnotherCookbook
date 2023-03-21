dee4baa7aa5d8cd13040a9d6a116c2924dae272d
Nick Arakaki <nicholas.w.arakaki@gmail.com>
2023-03-21 11:46:09 -0500

    add Ingredient model

---
+++ app/models/ingredient.py
@@ -0,0 +1,13 @@
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+
+class Ingredient(db.Model):
+    __tablename__ = "ingredients"
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    recipe_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("recipes.id")), nullable=False)
+    ingredient = db.Column(db.String, nullable=False)
+    amount = db.Column(db.Float, nullable=False)
+    units = db.Column(db.String, nullable=False)
