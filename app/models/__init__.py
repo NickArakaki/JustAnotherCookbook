9dd3a53a6209877650f7f071a5b3547f862d6692
Nick Arakaki <nicholas.w.arakaki@gmail.com>
2023-03-21 12:44:59 -0500

    add ingredients and methods migrations

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -2,3 +2,6 @@ from .db import db
 from .user import User
 from .db import environment, SCHEMA
 from .recipe import Recipe
+from .ingredient import Ingredient
+from .method import Method
+from .review import Review
