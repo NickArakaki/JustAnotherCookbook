bd7141cbc6c0ad7cd4f43d63250540c43df9bf31
Nick Arakaki <nicholas.w.arakaki@gmail.com>
2023-03-21 12:42:03 -0500

    create migrations for users and recipes

--- app/models/recipe.py
+++ app/models/recipe.py
@@ -1,5 +1,6 @@
 from .db import db, environment, SCHEMA, add_prefix_for_prod
 from datetime import datetime, timezone
+from sqlalchemy.sql import text
 
 class Recipe(db.Model):
     __tablename__ = "recipes"
@@ -11,8 +12,8 @@ class Recipe(db.Model):
     author_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("users.id")), nullable=False)
     total_time = db.Column(db.Integer, nullable=False)
     description = db.Column(db.String(500), nullable=False)
-    created_at = db.Column(db.Date, nullable=False, default=datetime(timezone.utc))
-    updated_at = db.Column(db.Date, nullable=False, default=datetime(timezone.utc))
+    created_at = db.Column(db.DateTime, nullable=False, server_default=text("now()"))
+    updated_at = db.Column(db.DateTime, nullable=False, server_default=text("now()"))
 
     reviews = db.relationship("Review", back_populates="recipe")
     author = db.relationship("User", back_populates="recipes")
