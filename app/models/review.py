0cbe9124970cd981dbd33da1301557b38950198c
Nick Arakaki <nicholas.w.arakaki@gmail.com>
2023-03-21 11:33:39 -0500

    add Review model

---
+++ app/models/review.py
@@ -0,0 +1,16 @@
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+from datetime import datetime, timezone
+
+class Review(db.Model):
+    __tablename__ = "reviews"
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("users.id")), nullable=False)
+    recipe_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("recipes.id")), nullable=False)
+    rating = db.Column(db.Integer, nullable=False)
+    review = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.Date, nullable=False, default=datetime(timezone.utc))
+    updated_at = db.Column(db.Date, nullable=False, default=datetime(timezone.utc))
