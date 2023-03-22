9405393fbde4c07a0b2e204887853a56a806d6c2
Nick Arakaki <nicholas.w.arakaki@gmail.com>
2023-03-22 11:10:37 -0500

    add GET_SINGLE_RECIPE slice to recipes reducer

--- react-app/src/store/recipes.js
+++ react-app/src/store/recipes.js
@@ -1,11 +1,19 @@
 // constants
 const GET_ALL_RECIPES = "recipes/GET_ALL_RECIPES"
+const GET_SINGLE_RECIPE = "/recipes/GET_SINGLE_RECIPE"
 
 // action creators
 const getAllRecipes = recipes => {
     return {
         type: GET_ALL_RECIPES,
         payload: recipes
+    }
+}
+
+const getSingleRecipe = recipe => {
+    return {
+        type: GET_SINGLE_RECIPE,
+        payload: recipe
     }
 }
 
@@ -29,19 +37,40 @@ export const getAllRecipesThunk = () => async (dispatch) => {
     }
 }
 
+export const getSingleRecipeThunk = (recipeId) => async (dispatch) => {
+    const res = await fetch(`/api/recipes/${recipeId}`);
+
+    if (res.ok) {
+        const data = await res.json();
+        dispatch(getSingleRecipe(data))
+        return null
+    } else if (res.status < 500) {
+        const data = await res.json();
+        if (data.errors) {
+            return data.errors
+        }
+    } else {
+        return ["An error occured. Please try again later."]
+    }
+}
+
 // reducer
 const initialState = { allRecipes: {}, singleRecipe: {} }
 
 export default function reducer(state = initialState, action) {
+    const newState = { ...state };
     switch(action.type) {
         case GET_ALL_RECIPES: {
-            const newState = { ...state };
             newState.allRecipes = { ...state.allRecipes }
 
             for (const recipe of action.payload) {
                 newState.allRecipes[recipe.id] = recipe
             }
 
+            return newState;
+        }
+        case GET_SINGLE_RECIPE: {
+            newState.singleRecipe = action.payload
             return newState;
         }
         default:
