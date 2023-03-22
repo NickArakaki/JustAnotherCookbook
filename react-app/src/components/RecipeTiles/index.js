29b0f291a54d659d6e13ac3d2ff7e8ab69dd3808
Nick Arakaki <nicholas.w.arakaki@gmail.com>
2023-03-22 11:05:39 -0500

    add minor styling to RecipeTiles component

--- react-app/src/components/RecipeTiles/index.js
+++ react-app/src/components/RecipeTiles/index.js
@@ -1,20 +1,25 @@
 import { useSelector } from "react-redux";
-import { Link } from "react-router-dom"
-
+import { Link, Redirect, useHistory } from "react-router-dom"
+import "./RecipeTiles.css"
 
 function RecipeTiles() {
     const recipes = useSelector(state => state.recipes.allRecipes)
+    const history = useHistory();
+
+    const goToRecipeDetails = (recipe) => {
+        history.push(`/recipes/${recipe.id}`)
+    }
 
     return (
         <div className="recipe_tiles_div">
             {Object.values(recipes).map(recipe => {
                 return (
-                    <div className="recipe_tile">
+                    <div key={recipe.id} className="recipe_tile">
                         <div className="recipe_image">
                             <div className="recipe_favorite_button"></div>
                         </div>
-                        <div className="recipe_title">{recipe.title}</div>
-                        <div className="recipe_description">{recipe.description}</div>
+                        <div onClick={() => goToRecipeDetails(recipe)} className="recipe_title">{recipe.title}</div>
+                        <div onClick={() => goToRecipeDetails(recipe)} className="recipe_description">{recipe.description}</div>
                         <div className="recipe_author">submitted by <Link to={`/users/${recipe.author.id}`}>{recipe.author.username}</Link></div>
                         <div className="recipe_tags"></div>
                     </div>
