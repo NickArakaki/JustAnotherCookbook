1f6a27dc8e23333900b369670dec1bcddda80534
Nick Arakaki <nicholas.w.arakaki@gmail.com>
2023-03-27 22:15:15 -0500

    randomly assign 5 tags to recipes in seed data

--- app/seeds/tags.py
+++ app/seeds/tags.py
@@ -1,9 +1,13 @@
-from app.models import db, Tag, environment, SCHEMA
+from app.models import db, Tag, Recipe, environment, SCHEMA
 from sqlalchemy.sql import text
+import random
 
 def seed_tags():
     tags = ["chocolate", "cookie", "dessert", "healthy", "entree", "lunch", "breakfast", "snack", "sandwich", "vegetarian", "soup"]
     db.session.add_all([Tag(tag=tag) for tag in tags])
+
+    recipes = Recipe.query.all()
+    [recipe.tags.extend(random.sample(Tag.query.all(), 5)) for recipe in recipes]
     db.session.commit()
 
 
