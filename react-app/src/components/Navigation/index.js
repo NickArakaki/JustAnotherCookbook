302353e51418e774e703c092dad6ba69cc7bb363
Nick Arakaki <nicholas.w.arakaki@gmail.com>
2023-04-06 17:15:41 -0500

    extract search component to funcitonal component

--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -3,10 +3,11 @@ import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import './Navigation.css';
 import ProfileButton from './ProfileButton';
+import SearchBar from './Search';
 
 function Navigation({ isLoaded }){
 	const sessionUser = useSelector(state => state.session.user);
-	const [searchInput, setSearchInput] = useState("")
+
 
 	return (
 		<div className='navbar_div'>
@@ -25,23 +26,7 @@ function Navigation({ isLoaded }){
 				}
 			</div>
 			<NavLink exact to="/"><span className='navbar_section navbar_logo'>JustAnotherCookbook</span></NavLink>
-			<div className='navbar_section navbar_search_div'>
-				<form onSubmit={(e) => {
-					e.preventDefault();
-					alert("Serch Feature Coming Soon!")
-					setSearchInput("")
-					}}
-					className='search_form'>
-					<input
-						className='search_form_input'
-						value={searchInput}
-						onChange={(e) => setSearchInput(e.target.value)}
-						type='text'
-						placeholder='Recipe Search'
-					/>
-					<i className="fa-solid fa-magnifying-glass" />
-				</form>
-			</div>
+			<SearchBar />
 		</div>
 	);
 }
